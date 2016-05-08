5f3935b30038dda38bb993851b4b0499201631ce
kawamura <mituhiro.kawamura@gmail.com>
2016-05-09 04:20:27 +0900

    追加

--- main.js
+++ main.js
@@ -2,7 +2,15 @@
   $(document).ready(function(){
     $(".tasks").sortable({
       connectWith: ".tasks",
-      placeholder: "on_tasks"
+      placeholder: "on_tasks",
+      update: function(ev, ui) {
+      //  var sort = $(".tasks").sortable(["serialize"], { attribute: 'p-id' });
+      //  console.log(sort);
+        $(".tasks").each(function(index, element){
+         var result = $(element).sortable("toArray", { attribute: 'p-id' });
+        console.log(result);
+        });
+      }
     });
   });
 })(jQuery);
